81a1231ed215193da471f27fac3b551696fe78cb
asuresh0707 <suresh.alaparti@gmail.com>
2018-03-17 09:09:53 +0000

    adding final keyword to greet method

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,7 @@ public class Greeter {
 
   }
 
-  public String greet(final String someone) {
+  public String final greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
